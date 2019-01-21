555fff9cba882ce2d9651bfb34a974fe4663bb36
Raúl <raul2040@outlook.es>
2019-01-21 10:52:50 +0100

    Primer commit, creado el componente toolbar, falta añadir lógica y estilo

---
+++ src/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Toolbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <div>
+                <h1>Hola esto es una prueba</h1>
+            </div>
+        )
+    }
+}
+
+export default Toolbar;
