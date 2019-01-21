2f844f8aa69082ea79e478ea02522a8d1a8ddb65
Raúl <raul2040@outlook.es>
2019-01-21 12:44:29 +0100

    Index pasado a React con éxito

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Toolbar from './Toolbar/Toolbar';
+import QuestionComponent from './QuestionComponent/QuestionComponent';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Toolbar/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Toolbar/>
+                <QuestionComponent title={"Cuál es tu super héroe Favorito"}/>
+            </div>
+        );
+    }
 }
 
 export default App;
