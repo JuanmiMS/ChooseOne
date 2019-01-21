df000bca249c5813005b796418f883841437332d
Raúl <raul2040@outlook.es>
2019-01-21 11:43:40 +0100

    Toolbar pasada a React

--- src/Toolbar/Toolbar.js
+++ src/Toolbar/Toolbar.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import {
+    Collapse,
+    Nav,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
 
 class Toolbar extends Component {
     constructor(props) {
@@ -10,46 +17,91 @@ class Toolbar extends Component {
             isOpen: false
         };
     }
+
     toggle() {
         this.setState({
             isOpen: !this.state.isOpen
         });
     }
+
     render() {
         return (
-            <div>
-                <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">reactstrap</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/components/">Components</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                            </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>
-                                    Options
-                                </DropdownToggle>
-                                <DropdownMenu right>
-                                    <DropdownItem>
-                                        Option 1
-                                    </DropdownItem>
-                                    <DropdownItem>
-                                        Option 2
-                                    </DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem>
-                                        Reset
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+
+                    <a className="navbar-brand" href="#">ChooseOne</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            </li>
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <Collapse isOpen={this.state.isOpen} navbar>
+                                <Nav className="nav-item dropdown" navbar>
+                                    <UncontrolledDropdown nav inNavbar>
+                                        <DropdownToggle nav caret>
+                                            DropDown
+                                        </DropdownToggle>
+                                        <DropdownMenu right>
+                                            <DropdownItem>
+                                                Action
+                                            </DropdownItem>
+                                            <DropdownItem>
+                                                Another action
+                                            </DropdownItem>
+                                            <DropdownItem divider/>
+                                            <DropdownItem>
+                                                Something else here
+                                            </DropdownItem>
+                                        </DropdownMenu>
+                                    </UncontrolledDropdown>
+                                    <UncontrolledDropdown nav inNavbar>
+                                        <DropdownToggle nav caret>
+                                            <strong>Nombre</strong>
+                                        </DropdownToggle>
+                                        <DropdownMenu right>
+                                            <DropdownItem>
+                                                <p className="text-left"><strong>Nombre Apellido</strong></p>
+                                            </DropdownItem>
+                                            <DropdownItem>
+                                                <p className="text-left small">correoElectronico@email.com</p>
+                                            </DropdownItem>
+                                            <DropdownItem>
+                                                <p className="text-left">
+                                                    <a href="#" className="btn btn-primary btn-block btn-sm">Actualizar
+                                                        Datos</a>
+                                                </p>
+                                            </DropdownItem>
+                                            <DropdownItem divider/>
+                                            <DropdownItem>
+                                                <li>
+                                                    <div className="navbar-login navbar-login-session">
+                                                        <div className="row">
+                                                            <div className="col-12">
+                                                                <p>
+                                                                    <a href="#" className="btn btn-danger btn-block">Cerrar
+                                                                        Sesion</a>
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            </DropdownItem>
+                                        </DropdownMenu>
+                                    </UncontrolledDropdown>
+                                </Nav>
+                            </Collapse>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
